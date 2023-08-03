3163812e3bc118d64ca1b69d56d7f20d3d037e3e
EricSaDev <eric.almeida.sa@gmail.com>
2023-08-03 09:43:48 -0300

    Editing Cors

--- angular-14-client/src/app/services/tutorial.service.ts
+++ angular-14-client/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://backend.demo-basa.apps.cluster-88z28.88z28.example.opentlc.com/api/tutorials';
+const baseUrl = 'http://backend-demo-basa.apps.cluster-88z28.88z28.example.opentlc.com/api/tutorials';
 
 @Injectable({providedIn: 'root'})
 export class TutorialService {
