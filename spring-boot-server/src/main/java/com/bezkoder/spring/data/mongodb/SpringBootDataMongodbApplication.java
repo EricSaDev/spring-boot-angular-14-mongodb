b354d80381c249dc0f94481144e1dbb5735b427a
EricSaDev <eric.almeida.sa@gmail.com>
2023-09-22 15:05:27 -0300

    Adding  Swagger

--- spring-boot-server/src/main/java/com/bezkoder/spring/data/mongodb/SpringBootDataMongodbApplication.java
+++ spring-boot-server/src/main/java/com/bezkoder/spring/data/mongodb/SpringBootDataMongodbApplication.java
@@ -3,7 +3,10 @@ package com.bezkoder.spring.data.mongodb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootDataMongodbApplication {
 
 	public static void main(String[] args) {
